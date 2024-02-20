39b096ed54f102049b40f7e73f8ec0469cb11854
David Strikaitis <davidstrik282@gmail.com>
2024-02-20 16:54:48 +0000

    added level being properly formatted, need to add sprites for it

---
+++ Animation/AnimationGame/AnimationGame/Source.cpp
@@ -0,0 +1,346 @@
+// platform game.....
+// Author :Noel O' Hara
+
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h> 
+
+class Game
+{
+public:
+	int slowMoTimeLeft = 0;
+	int ExtraJumpTimeLeft = 0;
+	sf::Font Font;
+	sf::Text winText;
+	sf::Sprite m_playerS;
+	sf::Texture m_playerT;
+	//create Window
+	sf::RenderWindow window;
+	sf::View view;
+	float randomNum;
+	float delay{ 0.125f };
+	float frameTimer{ 0.f };
+	int currentFrame{ 0 };
+	int frameHeight{ 0 };
+
+	sf::RectangleShape playerShape;
+
+	float velocityX = 0, velocityY = 0, gravity = 0.3;
+
+	static const int numRows = 20;
+	static const int numCols = 45;
+	int levelData[numRows][numCols] = {
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 2 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 4 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 4 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 5 , 0 , 0 , 2 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 5 , 0 , 5 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 3 },
+	{ 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 , 1 },
+	{ 0 , 0 , 0 , 0 , 0 , 1 , 0 , 2 , 0 , 2 , 0 , 0 , 0 , 0 , 5 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 5 , 0 , 0 , 0 },
+	{ 1 , 1 , 1 , 1 , 1 , 1 , 1 , 2 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0 }, };
+
+	sf::RectangleShape level[numRows][numCols];
+
+	Game()
+	{
+		window.create(sf::VideoMode(800, 600), "Endless Runner Game");
+		if(!Font.loadFromFile("PixelSans.ttf"))
+		{
+			std::cout << "DIDNT LOAD TEXT\n";
+		}
+		winText.setFont(Font);
+		winText.setPosition(200.f, 300.f);
+		winText.setCharacterSize(32u);
+		winText.setString("you win");
+		winText.setFillColor(sf::Color::Transparent);
+		if (!m_playerT.loadFromFile("player.png"))
+		{
+			std::cout << "DIDNT LOAD Player\n";
+		}
+		m_playerS.setTexture(m_playerT);
+		m_playerS.setTextureRect(sf::IntRect(1,1,186,171));
+		m_playerS.setScale(0.125f, 0.125f);
+	}
+	void init()
+	{
+		slowMoTimeLeft = 0;
+		ExtraJumpTimeLeft = 0;
+		winText.setFillColor(sf::Color::Transparent);
+
+		view = window.getDefaultView();
+		playerShape.setSize(sf::Vector2f(20, 20));
+		playerShape.setPosition(160, 500);
+		m_playerS.setPosition(160, 500);
+		m_playerS.setPosition(playerShape.getPosition());
+
+		for (int col = 0; col < numCols; col++)
+		{
+			for (int row = 0; row < numRows; row++)
+			{
+
+				if (levelData[row][col] == 1)
+				{
+
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+					level[row][col].setFillColor(sf::Color::Red);
+				}
+				if (levelData[row][col] == 0)
+				{
+
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+					level[row][col].setFillColor(sf::Color::Black);
+				}
+				if (levelData[row][col] == 2)
+				{
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+
+					level[row][col].setFillColor(sf::Color::Blue);
+
+				}
+				if (levelData[row][col] == 3)
+				{
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+
+					level[row][col].setFillColor(sf::Color::Green);
+				}
+				
+				if (levelData[row][col] == 4)
+				{
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+
+					level[row][col].setFillColor(sf::Color::Magenta);
+
+				}
+				if (levelData[row][col] == 5)
+				{
+					level[row][col].setSize(sf::Vector2f(70, 30));
+					level[row][col].setPosition(col * 70, row * 30);
+
+					level[row][col].setFillColor(sf::Color::Yellow);
+
+				}
+
+			}
+			std::cout << std::endl;
+		}
+
+	}
+	void run()
+	{
+
+
+		sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
+
+
+		sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
+
+		sf::Clock clock;
+
+		clock.restart();
+
+		while (window.isOpen())
+		{
+			// events =======================================================================
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			timeSinceLastUpdate += clock.restart();
+
+			if (timeSinceLastUpdate > timePerFrame)
+			{ // update =====================================================================
+				if (slowMoTimeLeft > 0)
+					slowMoTimeLeft--;
+				if (ExtraJumpTimeLeft > 0)
+					ExtraJumpTimeLeft--;
+
+				frameTimer += delay;
+				if (static_cast<int>(frameTimer) >= 1)
+				{
+					frameTimer = 0;
+					currentFrame++;
+					if (currentFrame > 7)
+						currentFrame = 0;
+					m_playerS.setTextureRect(sf::IntRect((171 * currentFrame), 1 + (173 * currentFrame), 173, 171));
+
+				}
+
+				for (int row = 0; row < numRows; row++)
+				{
+					for (int col = 0; col < numCols; col++)
+					{
+						if (slowMoTimeLeft <= 0)
+							level[row][col].move(-3.7, 0);
+						else
+							level[row][col].move(-2.4, 0);
+					}
+
+				}
+
+
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && (velocityY == 0 || ExtraJumpTimeLeft > 0))
+				{
+					if (velocityY != 0 && ExtraJumpTimeLeft > 0)
+					{
+						velocityY = -23;
+						ExtraJumpTimeLeft = 0;
+					}
+					else
+						velocityY = -11.8;
+				}
+
+				velocityY = velocityY + gravity;
+				playerShape.move(0, velocityY);
+
+				m_playerS.setPosition(playerShape.getPosition());
+
+				gravity = 0.6;
+
+				for (int row = 0; row < numRows; row++)
+				{
+					for (int col = 0; col < numCols; col++)
+					{
+						if (velocityY >= 0)
+						{
+							if (levelData[row][col] == 1)
+							{
+
+								if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+								{
+									if (playerShape.getPosition().y < level[row][col].getPosition().y)
+									{
+										frameHeight = 0;
+										gravity = 0;
+										velocityY = 0;
+										playerShape.setPosition(playerShape.getPosition().x, level[row][col].getPosition().y);
+										playerShape.move(0, -playerShape.getGlobalBounds().height);
+										break;
+									}
+									else
+									{
+										init();
+									}
+								}
+
+
+							}
+
+						}
+						if (velocityY < 0)
+						{
+							if (levelData[row][col] == 1)
+							{
+								if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+								{
+									init();
+								}
+
+							}
+
+						}
+						if (levelData[row][col] == 2)
+						{
+							if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+							{
+								init();
+							}
+						}
+						if (levelData[row][col] == 3)
+						{
+							if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+							{
+								winText.setFillColor(sf::Color::White);
+							}
+						}
+						if (levelData[row][col] == 4)
+						{
+							if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+							{
+								slowMoTimeLeft = 360;
+							}
+						}
+						if (levelData[row][col] == 5)
+						{
+							if (playerShape.getGlobalBounds().intersects(level[row][col].getGlobalBounds()))
+							{
+								ExtraJumpTimeLeft = 100;
+							}
+						}
+					}
+				}
+				if (velocityY > 0.7)
+					frameHeight = 1;
+
+				if (playerShape.getPosition().y > 600)
+				{
+					init();
+				}
+
+				window.clear();
+
+				for (int row = 0; row < numRows; row++)
+				{
+					for (int col = 0; col < numCols; col++)
+					{
+						window.draw(level[row][col]);
+
+					}
+				}
+				window.draw(playerShape);
+				window.draw(m_playerS);
+
+				window.draw(winText);
+
+				window.display();
+
+				timeSinceLastUpdate = sf::Time::Zero;
+			}
+		}
+	}
+};
+
+
+int main()
+{
+	Game game;
+	game.init();
+	game.run();
+
+	return 0;
+}
